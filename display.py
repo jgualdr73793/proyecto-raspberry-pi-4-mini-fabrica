574e1e16d10c0bade0374f7dc204db9d76332f8b
Jgualdr73793 <jufeguru@gmail.com>
2022-10-19 16:08:09 -0500

    se corrigio la linea 8 y 9

--- display.py
+++ display.py
@@ -5,8 +5,8 @@ display=lcddriver.lcd()
 try:
   while True:
     print("toy funcionando")
-    display.cd_display_string("linea uno",1)
-    display.lcd_dislpay_string("linea dos"2)
+    display.lcd_display_string("linea uno",1)
+    display.lcd_dislpay_string("linea dos",2)
     time.sleep(2)
     display.lcd_display_string("prueba de inciio al teclado")
     time.sleep(2)
