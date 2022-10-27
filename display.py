180edbb9d81e8e3636d6cdcc2dd3380e898f14a2
Jgualdr73793 <jufeguru@gmail.com>
2022-10-27 10:13:27 -0500

    se creo main.py y hizo el programa que une el teclado con el display en este

--- display.py
+++ display.py
@@ -1,22 +1,8 @@
 import lcddriver
 import time
-import tecladop as Tl
 
 dis= lcddriver.lcd()
 
-def display(Tl):
-  if Tl.lectura=="1":
-    dis.lcd_display_string("se pulso numero 1",1)
-    time.sleep(4)
-  if Tl.lectura=="2":
-    dis.lcd_display_string("se pulso numero 2",1)
-    time.sleep(4)
-  if Tl.lectura=="3":
-    dis.lcd_display_string("se pulso numero 3",1)
-    time.sleep(4)
-  if Tl.lectura=="4":
-    dis.lcd_display_string("se pulso numero 4",1)
-    time.sleep(4)
 
 
 
