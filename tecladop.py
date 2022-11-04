5aedb531f1c234db2ee653c7be70402ee042e644
Jgualdr73793 <jufeguru@gmail.com>
2022-11-04 13:25:48 -0500

    se agrego cambios y se agregaron los archivos loop y menu

--- tecladop.py
+++ tecladop.py
@@ -7,28 +7,22 @@ fila=13
 GPIO.setup(13,GPIO.OUT)
 
 columnas=[8,10,11,12]
-GPIO.setup (8,GPIO.IN, GPIO,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup (10,GPIO.IN, GPIO,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup (11,GPIO.IN, GPIO,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup (12,GPIO.IN, GPIO,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup (8,GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup (10,GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup (11,GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup (12,GPIO.IN, GPIO.PUD_DOWN)
 
 def lectura():
     GPIO.output(fila,GPIO.HIGH)
     if GPIO.input(columnas[0])==1:
-        return "1"
+        return 1
     if GPIO.input(columnas[1])==1:
-        return "2"
+        return 2
     if GPIO.input(columnas[2])==1:
-        return "3"
+        return 3
     if GPIO.input(columnas[3])==1:
-        return "4"
+        return 4
     GPIO.output(fila,GPIO.LOW)
-    return "0"
-
-x=""
-while True:
-    x=(lectura())
-    time.sleep(0.1)
 
 
 # Path: main.py
